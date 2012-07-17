ba379cfb95ef69413dc61db7afdf378ff0236f37
kawakami <kawakami@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2012-07-17 09:03:40 +0000

    insert temp data to new-blank MS
    git-svn-id: http://alma-dms/svn/sakura/trunk@69 9a79bd28-2a0b-46f8-beb7-14fc758f186d

---
+++ bench/A/insertToPointing.cc
@@ -0,0 +1,207 @@
+#include <sys/time.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <cstdio>
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <memory>
+#include <iostream>
+#include <iomanip>
+
+#include <casacore/casa/aips.h>
+#include <casacore/casa/Inputs/Input.h>
+#include <casacore/casa/BasicSL/String.h>
+#include <casacore/casa/Utilities/CountedPtr.h>
+#include <casacore/casa/OS/Directory.h>
+#include <casacore/tables/Tables/Table.h>
+#include <casacore/ms/MeasurementSets.h>
+
+#include <tables/Tables/ExprNode.h>
+#include <tables/Tables/TableDesc.h>
+#include <tables/Tables/SetupNewTab.h>
+#include <tables/Tables/TableIter.h>
+#include <tables/Tables/RefRows.h>
+#include <tables/Tables/TableRow.h>
+
+
+#define unique_ptr auto_ptr
+#define enter() do { cout << "Enter: " << __FUNCTION__ << endl; } while (0)
+#define elementsof(x) (sizeof(x) / sizeof(*(x)))
+
+using namespace std;
+using namespace casa;
+
+namespace {
+char const ENV_VAR_SAKURA_ROOT[] = "SAKURA_ROOT";
+
+double currenttime() {
+  struct timeval tv;
+  //int result = gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + ((double)tv.tv_usec) / 1000000.;
+}
+
+void insertToPointing(char const*filename) {
+  enter();
+  MeasurementSet ms(filename);
+  MSPointing &tab = ms.pointing();
+  ROMSPointingColumns rocols(tab);
+
+  casa::MeasurementSet *mstable_ ;
+  TableDesc msDesc = MeasurementSet::requiredTableDesc() ;
+  SetupNewTable newtab( "kawakami.ms", msDesc, Table::New ) ;
+
+  mstable_ = new MeasurementSet( newtab ) ;
+  TableDesc pointingDesc = MSPointing::requiredTableDesc() ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::ANTENNA_ID) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::TIME) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::INTERVAL ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::NAME ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::NUM_POLY ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::TIME_ORIGIN ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::DIRECTION ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::TARGET ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::POINTING_OFFSET ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::SOURCE_OFFSET ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::ENCODER ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::POINTING_MODEL_ID ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::TRACKING ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::ON_SOURCE ) ;
+  MSPointing::addColumnToDesc( pointingDesc, MSPointingEnums::OVER_THE_TOP ) ;
+
+  SetupNewTable pointingTab( mstable_->pointingTableName(), pointingDesc, Table::New ) ;
+  mstable_->rwKeywordSet().defineTable( MeasurementSet::keywordName( MeasurementSet::POINTING ), Table( pointingTab ) ) ;
+  mstable_->initRefs();
+
+  uInt nrow = tab.nrow();
+
+  MSPointing mspo = mstable_->pointing() ;
+  mspo.addRow( nrow, True );
+  MSPointingColumns cols(mspo);
+
+  // ANTENNA_ID is always 0
+  Vector<Int> antIdArr( nrow, 0 ) ;
+  cols.antennaId().putColumn( antIdArr ) ;
+
+  Vector< Double > tmpVect(1);
+  tmpVect[0]=7.77;
+  Double intervalvalue=1.152;
+
+  double start = currenttime();
+  for (uInt i = 0; i < nrow; i++) {
+    //cols.antennaId().put(i,ante) ;
+    cols.time().put(i,2012.0) ;// TIME
+    cols.interval().put(i,intervalvalue) ;// INTERVAL
+    cols.name().put(i,"kawa-kami") ;// NAME
+    cols.numPoly().put(i,77);// NUM_POLY
+    cols.timeOrigin().put(i,0.314);// TIME_ORIGIN
+    //cols.direction().put(i,tmpVect);// DIRECTION
+    cols.target().put(i,tmpVect);// TARGET
+    cols.pointingOffset().put(i,tmpVect);//POINTING_OFFSET
+    cols.sourceOffset().put(i,tmpVect); //SOURCE_OFFSET
+    cols.encoder().put(i,tmpVect); // ENCODER
+    cols.pointingModelId().put(i,8);  // POINTING_MODEL_ID
+    cols.tracking().put(i,1); // TRACKING
+    cols.onSource().put(i,1); // ON_SOURCE
+    cols.overTheTop().put(i,1); // OVER_THE_TOP
+    }
+
+  // Finalize
+  MeasurementSet mstableTmp_ ;
+  mstableTmp_ = MeasurementSet( (*mstable_)) ;
+  delete mstable_ ;
+  mstableTmp_.closeSubTables() ;
+  mstableTmp_.unlock() ;
+
+  double end = currenttime();
+  cout << "Inserted: " << end - start << "sec\n";
+}
+
+// void insertToPointing(char const*filename) {
+//  enter();
+//  MeasurementSet ms(filename);
+//  insertToPointing_(ms);
+// }
+
+
+struct Entry {
+  char const *option;
+  void (*func)(char const*filename);
+} entries[] = {
+  {"pointing", insertToPointing}
+};
+
+char const *progName = "";
+
+void usage() {
+  cerr << "Usage: " << progName << " [options] action MSFile\n";
+  cerr << "options:: \n";
+  cerr << "\t--prefix path\tA path where sakura is installed.\n";
+  cerr << "\t-p path\n";
+  cerr << "\taction action is one of followings\n";
+  for (size_t i = 0; i < elementsof(entries); i++) {
+    cerr << "\t\t" << entries[i].option << endl;
+  }
+}
+
+}
+
+int main(int argc, char const * const argv[]) { 
+  progName = argv[0];
+  char const *prefix = PREFIX;
+
+  char const *root = getenv(ENV_VAR_SAKURA_ROOT);
+  if (root != NULL) {
+    prefix = root;
+  }
+
+  static struct option const long_options[] = {
+    {"prefix", 1, NULL, 'p'},
+    {0, 0, NULL, 0}
+  };
+
+  for (;;) {
+    int option_index = 0;
+    int optCh = getopt_long (argc, const_cast<char *const *>(argv), "p:",
+			     long_options, &option_index);
+    if (optCh == -1) {
+      break;
+    }
+    switch (optCh) {
+    case 'p':
+      prefix = optarg;
+      break;
+    case '?':
+      usage();
+      return 1;
+    default:
+      assert(false);
+      return 1;
+    }
+  }
+
+  int argStart = optind;
+  if (argc - argStart != 2) {
+    usage();
+    return 1;
+  }
+  cout << "SAKURA_ROOT: " << prefix << endl;
+  void (*func)(char const*filename) = NULL;
+  for (size_t i = 0; i < elementsof(entries); i++) {
+    if (strcmp(entries[i].option, argv[argStart]) == 0) {
+      func = entries[i].func;
+    }
+  }
+  if (func == NULL) {
+    usage();
+    return 1;
+  }
+  double start = currenttime();
+  func(argv[argStart + 1]);
+  double end = currenttime();
+  cout << "Total: " << end - start << "sec\n";
+  return 0;
+}
+
