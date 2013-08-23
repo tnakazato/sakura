6971daf9e9be94a0d6fa4eca02690dde49338acf
kana <kana@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-08-23 11:35:15 +0000

    Added longer tests, BitOperation32.AndLarge and BitOperation8.AndLarge. They invoke operation of a 262,144 element-array for 2,000 loops.
    git-svn-id: http://alma-dms/svn/sakura/trunk@200 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/test/bit_operation.cc
+++ libsakura/test/bit_operation.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sys/time.h>
 
 #include <libsakura/sakura.h>
 #include "aligned_memory.h"
@@ -7,6 +8,7 @@
 
 /* the number of elements in input/output array to test */
 #define NUM_IN 8
+#define NUM_LARGE 262144 //2**18
 
 using namespace std;
 
@@ -60,6 +62,13 @@ protected:
 				buff[bit_size-1-i] = '1';
 		}
 		return string(buff);
+	}
+
+	/* Get time of the day in unit of second */
+	double currenttime() {
+		struct timeval tv;
+		int result = gettimeofday(&tv, NULL);
+		return tv.tv_sec + ((double) tv.tv_usec) * 1.e-6;
 	}
 
 	/* Converts an bit pattern (char) to a value of DataType.*/
@@ -130,9 +139,6 @@ TEST_F(BitOperation8, And) {
 	uint8_t result[NUM_IN] = {0, 1, 2, 3, 0, 0, 2, 2};
 	//uint8_t result[NUM_IN];
 	size_t const num_in(NUM_IN);
-	// conversion between bit pattern to uint8_t
-	//char result_bit[NUM_IN][9] = {"00000000", "00000001", "00000010", "00000011", "00000000", "00000000", "00000010", "00000010"};
-	//getResultUInt8(true, NUM_IN, result_bit, result);
 
 	if (verbose) PrintInputs();
 
@@ -148,6 +154,42 @@ TEST_F(BitOperation8, And) {
 }
 
 /*
+ * Long test of bit operation AND by sakura_OperateBitsUint8And with a large array
+ * RESULT:
+ * out = [00000000, 00000001, 00000010, 00000011, 00000000, 00000000, 00000010, 00000010, .... repeated... ]
+ */
+TEST_F(BitOperation8, AndLarge) {
+	SIMD_ALIGN uint8_t out[NUM_LARGE];
+	SIMD_ALIGN uint8_t in[NUM_LARGE];
+	SIMD_ALIGN bool edit_mask[NUM_LARGE];
+	uint8_t result[NUM_IN] = {0, 1, 2, 3, 0, 0, 2, 2};
+	size_t const num_large(NUM_LARGE);
+	size_t const num_in(NUM_IN);
+	double start, end;
+	size_t const num_repeat = 2000;
+	LIBSAKURA_SYMBOL(Status) status;
+
+	for (size_t i = 0 ; i < num_large ; ++i){
+		in[i] = in_[i % num_in];
+		edit_mask[i] = edit_mask_[i % num_in];
+	}
+
+	cout << "Iterating " << num_repeat << " loops. The length of arrays is " << num_large << endl;
+	start = currenttime();
+	for (size_t i = 0 ; i < num_repeat ; ++i){
+		status = sakura_OperateBitsUint8And(bit_mask_, num_large, in, edit_mask, out);
+	}
+	end = currenttime();
+	cout << "Elapse time of actual operation: " << end - start << " sec" << endl;
+
+	// Verification
+	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
+	for (size_t i = 0 ; i < num_large ; ++i){
+		ASSERT_EQ(out[i], result[i % num_in]);
+	}
+}
+
+/*
  * Test bit operation AND by sakura_OperateBitsUint32And
  * RESULT:
  * out = [0...000, 0...001, 0...010, 0...011, 0...000, 0...000, 0...010, 0...010 ]
@@ -156,10 +198,6 @@ TEST_F(BitOperation32, And) {
 	SIMD_ALIGN uint32_t out[NUM_IN];
 	uint32_t result[NUM_IN] = {0, 1, 2, 3, 0, 0, 2, 2};
 	size_t const num_in(NUM_IN);
-	//uint32_t result[NUM_IN];
-	// conversion between bit pattern to uint8_t
-	//char result_bit[NUM_IN][9] = {"00000000", "00000001", "00000010", "00000011", "00000000", "00000000", "00000010", "00000010"};
-	//getResultUInt(true, NUM_IN, result_bit, result);
 
 	if (verbose) PrintInputs();
 
@@ -173,3 +211,40 @@ TEST_F(BitOperation32, And) {
 		ASSERT_EQ(out[i], result[i]);
 	}
 }
+
+/*
+ * Long test of bit operation AND by sakura_OperateBitsUint32And with a large array
+ * RESULT:
+ * out = [0...000, 0...001, 0...010, 0...011, 0...000, 0...000, 0...010, 0...010, .... repeated... ]
+ */
+TEST_F(BitOperation32, AndLarge) {
+	SIMD_ALIGN uint32_t out[NUM_LARGE];
+	SIMD_ALIGN uint32_t in[NUM_LARGE];
+	SIMD_ALIGN bool edit_mask[NUM_LARGE];
+	uint32_t result[NUM_IN] = {0, 1, 2, 3, 0, 0, 2, 2};
+	size_t const num_large(NUM_LARGE);
+	size_t const num_in(NUM_IN);
+	double start, end;
+	size_t const num_repeat = 2000;
+	LIBSAKURA_SYMBOL(Status) status;
+
+	for (size_t i = 0 ; i < num_large ; ++i){
+		in[i] = in_[i % num_in];
+		edit_mask[i] = edit_mask_[i % num_in];
+	}
+
+	cout << "Iterating " << num_repeat << " loops. The length of arrays is " << num_large << endl;
+	start = currenttime();
+	for (size_t i = 0 ; i < num_repeat ; ++i){
+		status = sakura_OperateBitsUint32And(bit_mask_, num_large, in, edit_mask, out);
+	}
+	end = currenttime();
+	cout << "Elapse time of actual operation: " << end - start << " sec" << endl;
+
+	// Verification
+	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
+	for (size_t i = 0 ; i < num_large ; ++i){
+		ASSERT_EQ(out[i], result[i % num_in]);
+	}
+}
+
