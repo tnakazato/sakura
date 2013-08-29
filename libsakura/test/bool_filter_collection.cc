b7f39299719892b41baa089b786edea99902f915
kana <kana@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-08-29 09:45:55 +0000

    Added actual tests to bool_filter_collection. Minor improvements in bit_operation tests.
    git-svn-id: http://alma-dms/svn/sakura/trunk@219 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/test/bool_filter_collection.cc
+++ libsakura/test/bool_filter_collection.cc
@@ -7,18 +7,14 @@
 #include "gtest/gtest.h"
 
 /* the number of elements in input/output array to test */
-#define NUM_IN 16
-#define NUM_RANGE 2
+#define NUM_IN 8
+#define NUM_RANGE 2 // DO NOT MODIFY THIS!
 #define NUM_IN_LONG 262144 //2**18
 
 using namespace std;
 
 /*
  * A super class to test various bit operation of an value and array
- * INPUTS:
- * - data = []
- * - upper_bound = []
- * - lower_bound = []
  */
 template<typename DataType>
 class BoolFilter : public ::testing::Test
@@ -32,10 +28,6 @@ protected:
 
 	virtual void SetUp()
 	{
-//		for (size_t i = 0; i < NUM_IN; ++i){
-//			data_[i] = i % ntype; /* repeat bit pattern of *00, *01, *10, *11,... */
-//		}
-
 		// Initialize sakura
 		LIBSAKURA_SYMBOL(Status) status = LIBSAKURA_SYMBOL(Initialize)();
 		EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
@@ -47,78 +39,130 @@ protected:
 		LIBSAKURA_SYMBOL(CleanUp)();
 	}
 
+	virtual void PrepareInputs() = 0;
+
+	void PrintArray(char const *name, size_t num_in, DataType *in){
+		cout << name << " = [ ";
+		for (size_t i = 0; i < num_in-1; ++i)
+			cout << in[i] << ", " ;
+		cout << in[num_in-1] ;
+		cout << " ]" << endl;
+	}
+
+	void PrintArray(char const *name, size_t num_in, bool *in){
+		cout << name << " = [ ";
+		for (size_t i = 0; i < num_in-1; ++i)
+			cout << (in[i] ? "T" : "F") << ", " ;
+		cout << (in[num_in-1] ? "T" : "F") ;
+		cout << " ]" << endl;
+	}
+
 	SIMD_ALIGN DataType data_[NUM_IN];
-	DataType upper_[NUM_RANGE];
-	DataType lower_[NUM_RANGE];
+	SIMD_ALIGN DataType upper_[NUM_RANGE];
+	SIMD_ALIGN DataType lower_[NUM_RANGE];
 	bool verbose;
 };
 
 /*
  * Tests various bool filter generation using float array
  * INPUTS:
- * - data = []
- * - upper_bound = []
- * - lower_bound = []
+ * - data = [0., -0.5, -1.0, -0.5, 0., 0.5, 1.0, 0.5]
+ * - lower_bound = [-0.75, 0.25]
+ * - upper_bound = [-0.25, 0.75]
  */
 class BoolFilterFloat : public BoolFilter<float>
-{};
+{
+protected:
+	virtual void PrepareInputs() {
+		float const base_input[8] = {0., -0.5, -1.0, -0.5, 0., 0.5, 1.0, 0.5};
+		ASSERT_EQ(NUM_RANGE, 2);
+		lower_[0] = -0.75;
+		lower_[1] = 0.25;
+		upper_[0] = -0.25;
+		upper_[1] = 0.75;
+		for (size_t i=0 ; i<NUM_IN ; ++i){
+			data_[i] = base_input[i % 8];
+		}
+		if (verbose){
+			PrintArray("data", NUM_IN, data_);
+			PrintArray("lower_bound", NUM_RANGE, lower_);
+			PrintArray("upper_bound", NUM_RANGE, upper_);
+		}
+	}
+};
 
 /*
  * Tests various bool filter generation using int array
  * INPUTS:
- * - data = []
- * - upper_bound = []
- * - lower_bound = []
+ * - data = [0, -5, -10, -5, 0, 5, 10, 5]
+ * - lower_bound = [-7, 3]
+ * - upper_bound = [-3, 7]
  */
 class BoolFilterInt : public BoolFilter<int>
-{};
+{
+protected:
+	virtual void PrepareInputs() {
+		int const base_input[8] = {0, -5, -10, -5, 0, 5, 10, 5};
+		ASSERT_EQ(NUM_RANGE, 2);
+		lower_[0] = -7;
+		lower_[1] = 3;
+		upper_[0] = -3;
+		upper_[1] = 7;
+		for (size_t i=0 ; i<NUM_IN ; ++i){
+			data_[i] = base_input[i % 8];
+		}
+		if (verbose){
+			PrintArray("data", NUM_IN, data_);
+			PrintArray("lower_bound", NUM_RANGE, lower_);
+			PrintArray("upper_bound", NUM_RANGE, upper_);
+		}
+	}
+};
 
 /*
  * Test bool filter generation sakura_SetTrueFloatInRangesInclusive
  * RESULT:
- * out = []
+ * out = [F, T, F, T, F, T, F, T]
  */
 TEST_F(BoolFilterFloat, RangesInclusive) {
 	SIMD_ALIGN bool out[NUM_IN];
-	bool answer[NUM_IN];// = {0, 1, 2, 3, 0, 0, 2, 2};
-	//uint8_t result[NUM_IN];
+	bool answer[8] = {false, true, false, true, false, true, false, true};
 	size_t const num_in(NUM_IN), num_range(NUM_RANGE);
 
-//	if (verbose) PrintInputs();
+	PrepareInputs();
 
 	LIBSAKURA_SYMBOL(Status) status = sakura_SetTrueFloatInRangesInclusive(num_in, data_, num_range, lower_, upper_, out);
 
-//	if (verbose) PrintArray("out", num_in, out);
+	if (verbose) PrintArray("out", num_in, out);
 
 	// Verification
 	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
-//	for (size_t i = 0 ; i < num_in ; ++i){
-//		ASSERT_EQ(out[i], answer[i]);
-//	}
+	for (size_t i = 0 ; i < num_in ; ++i){
+		ASSERT_EQ(out[i], answer[i % 8]);
+	}
 }
 
 /*
  * Test bool filter generation sakura_SetTrueIntInRangesInclusive
  * RESULT:
- * out = []
+ * out = [F, T, F, T, F, T, F, T]
  */
 TEST_F(BoolFilterInt, RangesInclusive) {
 	SIMD_ALIGN bool out[NUM_IN];
-	bool answer[NUM_IN];// = {0, 1, 2, 3, 0, 0, 2, 2};
-	//uint8_t result[NUM_IN];
+	bool answer[8] = {false, true, false, true, false, true, false, true};
 	size_t const num_in(NUM_IN), num_range(NUM_RANGE);
 
-//	if (verbose) PrintInputs();
+	PrepareInputs();
 
 	LIBSAKURA_SYMBOL(Status) status = sakura_SetTrueIntInRangesInclusive(num_in, data_, num_range, lower_, upper_, out);
 
-//	if (verbose) PrintArray("out", num_in, out);
+	if (verbose) PrintArray("out", num_in, out);
 
 	// Verification
 	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
-//	for (size_t i = 0 ; i < num_in ; ++i){
-//		ASSERT_EQ(out[i], answer[i]);
-//	}
+	for (size_t i = 0 ; i < num_in ; ++i){
+		ASSERT_EQ(out[i], answer[i % 8]);
+	}
 }
 
 
