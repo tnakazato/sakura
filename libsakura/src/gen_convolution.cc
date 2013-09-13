9cbe038e63174746ce65ddf3d70d4bd033c43104
kawakami <kawakami@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-09-13 03:05:02 +0000

    formatted by eclipse.
    git-svn-id: http://alma-dms/svn/sakura/trunk@304 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/gen_convolution.cc
+++ libsakura/src/gen_convolution.cc
@@ -7,44 +7,48 @@
 #include "libsakura/localdef.h"
 
 extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(CreateConvolve1DContext)(
-		             size_t num_channel,LIBSAKURA_SYMBOL(Convolve1DKernelType) kernel_type,
-		             size_t kernel_width,bool use_fft,LIBSAKURA_SYMBOL(Convolve1DContext) **context){
+		size_t num_channel, LIBSAKURA_SYMBOL(Convolve1DKernelType) kernel_type,
+		size_t kernel_width, bool use_fft,
+		LIBSAKURA_SYMBOL(Convolve1DContext) **context) {
 	try {
-	auto createconv =
-			::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
-	createconv->CreateConvolve1DContext(num_channel,kernel_type,kernel_width,use_fft,context);
+		auto createconv =
+				::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
+		createconv->CreateConvolve1DContext(num_channel, kernel_type,
+				kernel_width, use_fft, context);
 	} catch (...) {
-			assert(false); // no exception should not be raised for the current implementation.
-			return LIBSAKURA_SYMBOL(Status_kUnknownError);
-		}
+		assert(false); // no exception should not be raised for the current implementation.
+		return LIBSAKURA_SYMBOL(Status_kUnknownError);
+	}
 	return LIBSAKURA_SYMBOL(Status_kOK);
 }
 
 extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(Convolve1D)(
-		             LIBSAKURA_SYMBOL(Convolve1DContext) **context,
-				     float input_spectrum[/*num_channels*/],bool const input_flag[/*num_channels*/],
-				     float output_spectrum[/*num_channels*/]){
+LIBSAKURA_SYMBOL(Convolve1DContext) **context,
+		float input_spectrum[/*num_channels*/],
+		bool const input_flag[/*num_channels*/],
+		float output_spectrum[/*num_channels*/]) {
 
 	try {
-	auto createconv =
-			::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
-	createconv->Convolve1D(context,input_spectrum,input_flag,output_spectrum);
+		auto createconv =
+				::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
+		createconv->Convolve1D(context, input_spectrum, input_flag,
+				output_spectrum);
 	} catch (...) {
-				assert(false); // no exception should not be raised for the current implementation.
-				return LIBSAKURA_SYMBOL(Status_kUnknownError);
-			}
+		assert(false); // no exception should not be raised for the current implementation.
+		return LIBSAKURA_SYMBOL(Status_kUnknownError);
+	}
 	return LIBSAKURA_SYMBOL(Status_kOK);
 }
 
 extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(DestroyConvolve1DContext)(
-		             LIBSAKURA_SYMBOL(Convolve1DContext) *context){
+LIBSAKURA_SYMBOL(Convolve1DContext) *context) {
 	try {
-	auto createconv =
-			::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
-	createconv->DestroyConvolve1DContext(context);
+		auto createconv =
+				::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetConvolutionImpl();
+		createconv->DestroyConvolve1DContext(context);
 	} catch (...) {
-			assert(false); // no exception should not be raised for the current implementation.
-			return LIBSAKURA_SYMBOL(Status_kUnknownError);
-		}
+		assert(false); // no exception should not be raised for the current implementation.
+		return LIBSAKURA_SYMBOL(Status_kUnknownError);
+	}
 	return LIBSAKURA_SYMBOL(Status_kOK);
 }
