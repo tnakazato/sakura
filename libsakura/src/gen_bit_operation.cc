bbeb1fbdf8117c7847d17e7ca2d16f07d74227f9
kana <kana@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-09-27 06:56:38 +0000

    Removed obsolete codes.
    git-svn-id: http://alma-dms/svn/sakura/trunk@391 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/gen_bit_operation.cc
+++ libsakura/src/gen_bit_operation.cc
@@ -10,8 +10,6 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(OperateBitsUint8And)(
 		uint8_t bit_mask, size_t num_data, uint8_t const data[],
 		bool const edit_mask[], uint8_t result[]) {
 	// Check parameter arguments.
-//	if (num_data == 0)
-//		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (data == nullptr)
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (result == nullptr)
@@ -24,15 +22,8 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(OperateBitsUint8And)(
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (!( LIBSAKURA_SYMBOL(IsAligned)(edit_mask)))
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
-	/* need to include CHECK_ARGS defined in gen_gridding.cc
-	 CHECK_ARGS(in != nullptr);
-	 CHECK_ARGS(out != nullptr);
-	 CHECK_ARGS(edit_mask != nullptr);
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(in));
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(out));
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(edit_mask));
-	 */
 
+	// Now actual operation
 	auto bitop =
 			::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetBitOperationImplUint8();
 	bitop->OperateBitsAnd(bit_mask, num_data, data, edit_mask, result);
@@ -44,8 +35,6 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(OperateBitsUint32And)(
 		uint32_t bit_mask, size_t num_data, uint32_t const data[],
 		bool const edit_mask[], uint32_t result[]) {
 	// Check parameter arguments.
-//	if (num_data == 0)
-//		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (data == nullptr)
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (result == nullptr)
@@ -58,15 +47,8 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(OperateBitsUint32And)(
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	if (!( LIBSAKURA_SYMBOL(IsAligned)(edit_mask)))
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
-	/* need to include CHECK_ARGS defined in gen_gridding.cc
-	 CHECK_ARGS(in != nullptr);
-	 CHECK_ARGS(out != nullptr);
-	 CHECK_ARGS(edit_mask != nullptr);
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(in));
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(out));
-	 CHECK_ARGS(LIBSAKURA_SYMBOL(IsAligned)(edit_mask));
-	 */
 
+	// Now actual operation
 	auto bitop =
 			::LIBSAKURA_PREFIX::OptimizedImplementationFactory::GetFactory()->GetBitOperationImplUint32();
 	bitop->OperateBitsAnd(bit_mask, num_data, data, edit_mask, result);
