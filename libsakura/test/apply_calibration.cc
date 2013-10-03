796fc21a53142f9d67e87384a218a2c2bcdc853f
nakazato <nakazato@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-10-03 01:36:04 +0000

    Added test for zero division. Refs #30
    git-svn-id: http://alma-dms/svn/sakura/trunk@411 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/test/apply_calibration.cc
+++ libsakura/test/apply_calibration.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sys/time.h>
+#include <math.h>
 
 #include <libsakura/sakura.h>
 #include <libsakura/localdef.h>
@@ -99,6 +100,28 @@ TEST_F(ApplyCalibrationTest, InvaidNumberOfScalingFactor) {
 	LIBSAKURA_SYMBOL(ApplyPositionSwitchCalibration)(num_scaling_factor,
 			scaling_factor, num_data, target, target, target);
 	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kInvalidArgument), status);
+}
+
+TEST_F(ApplyCalibrationTest, ZeroDivision) {
+	size_t const num_scaling_factor = 1;
+	size_t const num_data = 1;
+	SIMD_ALIGN
+	float scaling_factor[num_scaling_factor];
+	scaling_factor[0] = 1.0;
+	SIMD_ALIGN
+	float target[num_data];
+	target[0] = 1.0;
+	SIMD_ALIGN
+	float reference[num_data];
+	reference[0] = 0.0;
+	SIMD_ALIGN
+	float result[num_data];
+
+	LIBSAKURA_SYMBOL(Status) status =
+	LIBSAKURA_SYMBOL(ApplyPositionSwitchCalibration)(num_scaling_factor,
+			scaling_factor, num_data, target, reference, result);
+	EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
+	EXPECT_TRUE(isinf(result[0]));
 }
 
 TEST_F(ApplyCalibrationTest, BasicTest) {
