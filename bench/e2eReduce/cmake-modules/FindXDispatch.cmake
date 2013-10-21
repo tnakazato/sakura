402ab82e9f003246fdd7d66a7f4aef338c0f3b81
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-10-21 06:10:57 +0000

    enable multithreading
    git-svn-id: http://alma-dms/svn/sakura/trunk@477 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- bench/e2eReduce/cmake-modules/FindXDispatch.cmake
+++ bench/e2eReduce/cmake-modules/FindXDispatch.cmake
@@ -1,5 +1,5 @@
 set(xdispatch_install_prefix "/opt/xdispatch")
-find_path(XDISPATCH_INCLUDE_DIR xdispatch/dispatch.h PATHS ${xdispatch_install_prefix}/include)
+find_path(XDISPATCH_INCLUDE_DIR xdispatch/dispatch PATHS ${xdispatch_install_prefix}/include)
 find_library(XDISPATCH_LIBRARY NAMES xdispatch dispatch PATHS ${xdispatch_install_prefix}/lib)
 
 include(FindPackageHandleStandardArgs)
@@ -7,6 +7,8 @@ find_package_handle_standard_args(XDISPATCH DEFAULT_MSG XDISPATCH_LIBRARY XDISPA
 
 if(XDISPATCH_FOUND)
   set(XDISPATCH_LIBRARIES ${XDISPATCH_LIBRARY})
+  get_filename_component(XDISPATCH_LIBRARY_PATH "${XDISPATCH_LIBRARY}" PATH)
+  SET(XDISPATCH_EXE_LINKER_FLAGS "-L ${XDISPATCH_LIBRARY_PATH} -Wl,-rpath,${XDISPATCH_LIBRARY_PATH} -lxdispatch")
 else(XDISPATCH_FOUND)
   set(XDISPATCH_LIBRARIES)
 endif(XDISPATCH_FOUND)
--- bench/e2eReduce/src/main.cc
+++ bench/e2eReduce/src/main.cc
@@ -5,35 +5,74 @@
  *      Author: kohji
  */
 
+#include <iostream>
+#include <unistd.h>
 #include <log4cxx/logger.h>
 #include <log4cxx/propertyconfigurator.h>
+#include <xdispatch/dispatch>
 #include <libsakura/sakura.h>
 #include <casacore/ms/MeasurementSets/MeasurementSet.h>
 
 namespace {
 auto logger = log4cxx::Logger::getLogger("app");
 
-void e2eReduce(int argc, char const* const argv[]) {
-	LOG4CXX_INFO(logger, "e2eReduce");
+void SerializedJob(int i) {
+	std::cout << i << std::endl;
+}
+
+void ParallelJob(int i) {
+	sleep(i % 3);
+	xdispatch::main_queue().sync([=] {
+		SerializedJob(i);
+	});
+}
+
+void E2eReduce(int argc, char const* const argv[]) {
+	LOG4CXX_INFO(logger, "Enter: E2eReduce");
+	{
+		auto group = xdispatch::group();
+		for (int i = 0; i < 20; ++i) {
+			group.async([=] {
+				ParallelJob(i);
+			});
+		}
+		group.wait(xdispatch::time_forever);
+	}
+	xdispatch::main_queue().async([=] {
+		sleep(1);
+		std::cout << "finished\n";
+		exit(0);
+	});
+	LOG4CXX_INFO(logger, "Leave: E2eReduce");
+}
+
+void main_(int argc, char const* const argv[]) {
+	LOG4CXX_INFO(logger, "start");
+	sakura_Status result;
+	xdispatch::main_queue().sync([&] {
+		result = sakura_Initialize(nullptr, nullptr);
+	});
+	if (result == sakura_Status_kOK) {
+		try {
+			E2eReduce(argc, argv);
+		} catch (...) {
+			LOG4CXX_ERROR(logger, "Exception raised");
+		}
+		xdispatch::main_queue().sync([] {
+			sakura_CleanUp();
+		});
+	} else {
+		LOG4CXX_ERROR(logger, "Failed to initialize libsakura.");
+	}
 }
 
 }
 
 int main(int argc, char const* const argv[]) {
 	::log4cxx::PropertyConfigurator::configure("config.log4j");
-	LOG4CXX_INFO(logger, "start");
-	sakura_Status result = sakura_Initialize(nullptr, nullptr);
-	if (result == sakura_Status_kOK) {
-		try {
-			e2eReduce(argc, argv);
-		} catch(...) {
-			LOG4CXX_ERROR(logger, "Exception raised");
-			return 1;
-		}
-		sakura_CleanUp();
-	} else {
-		LOG4CXX_ERROR(logger, "Failed to initialize libsakura.");
-		return 1;
-	}
+	xdispatch::global_queue().async([=] {
+		main_(argc, argv);
+	});
+	xdispatch::exec(); // never returns
 	return 0;
 }
