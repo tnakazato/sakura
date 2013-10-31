27f860611b4284462cad5bde6616a56d024d9096
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-10-31 06:18:29 +0000

    Added AlignedAllocateOrException
    git-svn-id: http://alma-dms/svn/sakura/trunk@535 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/libsakura/memory_manager.h
+++ libsakura/src/libsakura/memory_manager.h
@@ -16,11 +16,14 @@
 #ifndef LIBSAKURA_LIBSAKURA_MEMORY_MANAGER_H_
 #define LIBSAKURA_LIBSAKURA_MEMORY_MANAGER_H_
 
+#include <new>
 #include <libsakura/sakura.h>
 
 namespace LIBSAKURA_PREFIX {
 class Memory {
 public:
+	typedef void (*TypeOfFree)(void*);
+
 	/**
 	 * @~
 	 * @brief
@@ -28,10 +31,12 @@ public:
 	 *
 	 * MT-safe
 	 *
-	 * @param size 0 is acceptable and a valid address for the area with size 0 would be returned.
+	 * @param[in] size 0 is acceptable and a valid address for the area with size 0 would be returned.
 	 */
 	static inline void * Allocate(size_t size) noexcept {
-		return allocator_(size);
+		void *ptr = allocator_(size);
+		// printf("Memory::Allocate: %p\n", ptr);
+		return ptr;
 	}
 	/**
 	 * @~
@@ -40,11 +45,42 @@ public:
 	 *
 	 * MT-safe
 	 *
-	 * @param ptr nullptr is acceptable.
+	 * @param[in] ptr nullptr is acceptable.
 	 */
 	static inline void Free(void *ptr) noexcept {
+		// printf("Memory::Free: %p\n", ptr);
 		deallocator_(ptr);
 	}
+
+	/**
+	 * @~
+	 * @brief
+	 * Allocate an aligned memory with @a size_in_bytes size and the aligned address is assigned to @a aligned_address
+	 *
+	 * @param[in] size_in_bytes size in bytes you requires, not a number of T.
+	 * @param[out] aligned_address @a *aligned_address points aligned memory on successful call, otherwise @a *aligned_address is set to nullptr.
+	 * @return the address of the top of allocated area which may be unaligned. This address is required when freeing the area by @ref Free(void *ptr).
+	 *
+	 * MT-safe
+	 */
+	template<typename T>
+	static inline void *AlignedAllocateOrException(size_t size_in_bytes,
+			T **aligned_address) throw (std::bad_alloc) {
+		assert(aligned_address != nullptr);
+		size_t alignment = LIBSAKURA_SYMBOL(GetAlignment)();
+
+		size_t const size_of_arena = size_in_bytes + alignment - 1;
+		void *ptr = Allocate(size_of_arena);
+		if (ptr == nullptr) {
+			*aligned_address = nullptr;
+			throw std::bad_alloc();
+		}
+		*aligned_address = reinterpret_cast<T *>(LIBSAKURA_SYMBOL(AlignAny)(
+				size_of_arena, ptr, size_in_bytes));
+		assert(*aligned_address != nullptr);
+		return ptr;
+	}
+
 private:
 	friend ::LIBSAKURA_SYMBOL(Status) (::LIBSAKURA_SYMBOL(Initialize))(
 			::LIBSAKURA_SYMBOL(UserAllocator) allocator,
