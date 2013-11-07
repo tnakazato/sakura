a20fd2a536664e0fd3e9423c6bc6de71a735ec02
wataru <wataru@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-11-07 10:01:41 +0000

    use alignDouble in sakura_GetBestFitModel().
    git-svn-id: http://alma-dms/svn/sakura/trunk@559 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/numeric_operation.cc
+++ libsakura/src/numeric_operation.cc
@@ -1,13 +1,14 @@
-#include <cassert>
-#include <cstddef>
-#include <cmath>
 #include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstddef>
 #include <iostream>
+#include <memory>
 
-#include "libsakura/localdef.h"
-#include "libsakura/memory_manager.h"
-#include "libsakura/optimized_implementation_factory_impl.h"
-#include "libsakura/sakura.h"
+#include <libsakura/localdef.h>
+#include <libsakura/memory_manager.h>
+#include <libsakura/optimized_implementation_factory_impl.h>
+#include <libsakura/sakura.h>
 
 #define EIGEN_DENSEBASE_PLUGIN "eigen_binary_visitor_plugin.h"
 #include <Eigen/Core>
@@ -30,33 +31,6 @@ inline void OperateFloatSubtraction(size_t num_in, float const *in1,
 
 	for (size_t i = 0; i < num_in; ++i) {
 		out[i] = in1[i] - in2[i];
-	}
-}
-
-inline void SolveSimultaneousEquationsByLU(size_t num_equations,
-		double const *lsq_matrix0, double const *lsq_vector0, double *out) {
-	assert(LIBSAKURA_SYMBOL(IsAligned)(lsq_matrix0));
-	assert(LIBSAKURA_SYMBOL(IsAligned)(lsq_vector0));
-	assert(LIBSAKURA_SYMBOL(IsAligned)(out));
-	Map<Array<double, Dynamic, 1>, Aligned> lsq_matrix0_array(const_cast<double *>(lsq_matrix0),
-			num_equations*num_equations);
-	Map<Array<double, Dynamic, 1>, Aligned> lsq_vector0_array(const_cast<double *>(lsq_vector0),
-			num_equations);
-	Map<Array<double, Dynamic, 1>, Aligned> out_array(const_cast<double *>(out),
-			num_equations);
-
-	::Eigen::MatrixXd lsq_matrix(num_equations, num_equations);
-	::Eigen::VectorXd lsq_vector(num_equations);
-	for (size_t i = 0; i < num_equations; ++i) {
-		for (size_t j = 0; j < num_equations; ++j) {
-			lsq_matrix(i, j) = lsq_matrix0_array[num_equations*i+j];
-		}
-		lsq_vector(i) = lsq_vector0_array[i];
-	}
-
-	::Eigen::VectorXd lu_result = lsq_matrix.fullPivLu().solve(lsq_vector);
-	for (size_t i = 0; i < num_equations; ++i) {
-		out_array[i] = lu_result(i);
 	}
 }
 
@@ -95,6 +69,33 @@ inline void GetCoefficientsForLeastSquareFitting(size_t num_data,
 	}
 }
 
+inline void SolveSimultaneousEquationsByLU(size_t num_equations,
+		double const *lsq_matrix0, double const *lsq_vector0, double *out) {
+	assert(LIBSAKURA_SYMBOL(IsAligned)(lsq_matrix0));
+	assert(LIBSAKURA_SYMBOL(IsAligned)(lsq_vector0));
+	assert(LIBSAKURA_SYMBOL(IsAligned)(out));
+	Map<Array<double, Dynamic, 1>, Aligned> lsq_matrix0_array(const_cast<double *>(lsq_matrix0),
+			num_equations*num_equations);
+	Map<Array<double, Dynamic, 1>, Aligned> lsq_vector0_array(const_cast<double *>(lsq_vector0),
+			num_equations);
+	Map<Array<double, Dynamic, 1>, Aligned> out_array(const_cast<double *>(out),
+			num_equations);
+
+	::Eigen::MatrixXd lsq_matrix(num_equations, num_equations);
+	::Eigen::VectorXd lsq_vector(num_equations);
+	for (size_t i = 0; i < num_equations; ++i) {
+		for (size_t j = 0; j < num_equations; ++j) {
+			lsq_matrix(i, j) = lsq_matrix0_array[num_equations*i+j];
+		}
+		lsq_vector(i) = lsq_vector0_array[i];
+	}
+
+	::Eigen::VectorXd lu_result = lsq_matrix.fullPivLu().solve(lsq_vector);
+	for (size_t i = 0; i < num_equations; ++i) {
+		out_array[i] = lu_result(i);
+	}
+}
+
 inline void DoGetBestFitModel(size_t num_data, size_t num_equations,
 		double const *model, double const *coeff, float *out) {
 
@@ -119,10 +120,27 @@ inline void GetBestFitModel(size_t num_data,
 	assert(LIBSAKURA_SYMBOL(IsAligned)(model));
 	assert(LIBSAKURA_SYMBOL(IsAligned)(out));
 
-	double *lsq_matrix0 = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases*num_model_bases));
-	//if (lsq_matrix0 == nullptr) {}
-	double *lsq_vector0 = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases));
-	double *coeff = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases));
+	size_t sakura_alignment = LIBSAKURA_SYMBOL(GetAlignment)();
+	size_t num_lsq_matrix0 = num_model_bases * num_model_bases;
+	size_t num_arena = num_lsq_matrix0 + sakura_alignment - 1;
+	std::unique_ptr<double[]> storage_for_lsq_matrix0(new double[num_arena]);
+	double *lsq_matrix0 = sakura_AlignDouble(num_arena,
+			storage_for_lsq_matrix0.get(), num_lsq_matrix0);
+	//double *lsq_matrix0 = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases*num_model_bases));
+
+	size_t num_lsq_vector0 = num_model_bases;
+	num_arena = num_lsq_vector0 + sakura_alignment - 1;
+	std::unique_ptr<double[]> storage_for_lsq_vector0(new double[num_arena]);
+	double *lsq_vector0 = sakura_AlignDouble(num_arena,
+			storage_for_lsq_vector0.get(), num_lsq_vector0);
+	//double *lsq_vector0 = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases));
+
+	size_t num_coeff = num_model_bases;
+	num_arena = num_coeff + sakura_alignment - 1;
+	std::unique_ptr<double[]> storage_for_coeff(new double[num_arena]);
+	double *coeff = sakura_AlignDouble(num_arena,
+			storage_for_coeff.get(), num_coeff);
+	//double *coeff = reinterpret_cast<double *>(LIBSAKURA_PREFIX::Memory::Allocate(sizeof(double)*num_model_bases));
 
 	GetCoefficientsForLeastSquareFitting(num_data, data, mask,
 			num_model_bases, model, lsq_matrix0, lsq_vector0);
@@ -132,9 +150,9 @@ inline void GetBestFitModel(size_t num_data,
 
 	DoGetBestFitModel(num_data, num_model_bases, model, coeff, out);
 
-	LIBSAKURA_PREFIX::Memory::Free(lsq_matrix0);
-	LIBSAKURA_PREFIX::Memory::Free(lsq_vector0);
-	LIBSAKURA_PREFIX::Memory::Free(coeff);
+	//LIBSAKURA_PREFIX::Memory::Free(lsq_matrix0);
+	//LIBSAKURA_PREFIX::Memory::Free(lsq_vector0);
+	//LIBSAKURA_PREFIX::Memory::Free(coeff);
 }
 
 } /* anonymous namespace */
