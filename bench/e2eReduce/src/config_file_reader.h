d93456c0aa34bb758fbdf0d3a02691a6d40c9280
nakazato <nakazato@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2013-11-15 09:17:52 +0000

    Add prototype of configuration file reader.
    git-svn-id: http://alma-dms/svn/sakura/trunk@608 9a79bd28-2a0b-46f8-beb7-14fc758f186d

---
+++ bench/e2eReduce/src/config_file_reader.h
@@ -0,0 +1,77 @@
+#ifndef _SAKURA_E2E_CONFIG_FILE_READER_H_
+#define _SAKURA_E2E_CONFIG_FILE_READER_H_
+
+#include <string>
+#include <fstream>
+namespace {
+
+class ConfigFileReader {
+public:
+	static void read(std::string const input_file) {
+		std::fstream f(input_file, std::fstream::in);
+		std::string s, key, value;
+		while (!f.eof()) {
+			std::getline(f, s);
+			if (s.size() == 0 || s.at(s.find_first_not_of(" \t")) == '#') {
+				//std::cout << "empty string or comment: " << s << std::endl;
+			} else {
+				//std::cout << "s=\"" << s << "\"" << std::endl;
+				SplitKeyAndValue(s, key, value);
+				std::cout << "key=\"" << key << "\", value=\"" << value << "\""
+						<< std::endl;
+				std::vector<std::string> key_list;
+				int n = SplitKey(key, key_list);
+				for (int i = 0; i < n; ++i) {
+					std::cout << "key_list[" << i << "]=" << key_list[i]
+							<< std::endl;
+				}
+			}
+		}
+	}
+
+	static std::string Trim(std::string const s) {
+	  std::string trimmed_string = "";
+	  std::string const trim_char_list(" \t\n");
+	  std::string::size_type left = s.find_first_not_of(trim_char_list);
+	  if (left != std::string::npos) {
+	    std::string::size_type right = s.find_last_not_of(trim_char_list);
+	    trimmed_string = s.substr(left,right-left+1);
+	  }
+	  return trimmed_string;
+	}
+
+	static void SplitKeyAndValue(std::string const &s, std::string &key, std::string &value) {
+	  std::string::size_type pos = s.find("=");
+	  if (pos == std::string::npos) {
+	    key = "";
+	    value = "";
+	  }
+	  else {
+	    key = Trim(s.substr(0,pos));
+	    value = Trim(s.substr(pos+1));
+	  }
+	}
+
+	static int SplitKey(std::string const key, std::vector<std::string> &key_list) {
+	  int count = 0;
+	  for (std::string::const_iterator i = key.begin(); i != key.end(); ++i) {
+	    count += (*i == '.') ? 1 : 0;
+	  }
+	  std::string::size_type start = 0;
+	  std::string::size_type end = std::string::npos;
+	  for (int i = 0; i < count; ++i) {
+	    end = key.find('.',start);
+	    key_list.push_back(key.substr(start,end-start));
+	    start = end + 1;
+	  }
+	  std::string s = key.substr(start);
+	  if (s.size() > 0) {
+	    key_list.push_back(s);
+	  }
+	  return static_cast<int>(key_list.size());
+	}
+};
+
+} /* anonymous namespace */
+
+#endif /* _SAKURA_E2E_CONFIG_FILE_READER_H_ */
