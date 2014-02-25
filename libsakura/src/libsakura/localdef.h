05efdc4716043d83e4e37ac6f53dabd98401f5e9
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2014-02-25 00:57:30 +0000

    Added likely, unlikely macro for optimization
    git-svn-id: http://alma-dms/svn/sakura/trunk@866 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/libsakura/localdef.h
+++ libsakura/src/libsakura/localdef.h
@@ -30,6 +30,16 @@
 #define ELEMENTSOF(x) (sizeof(x) / sizeof((x)[0]))
 #define STATIC_ASSERT(x) static_assert((x), # x)
 
+#undef likely
+#undef unlikely
+#if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
+# define likely(expr) (__builtin_expect(!!(expr), 1))
+# define unlikely(expr) (__builtin_expect(!!(expr), 0))
+#else
+# define likely(expr) (expr)
+# define unlikely(expr) (expr)
+#endif
+
 /*
  * ALIGNMENT must be a power of 2.
  */
