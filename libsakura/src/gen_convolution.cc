71e689337e3e8d8d07e18af273921d28f330eb81
kawakami <kawakami@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2014-03-19 01:01:33 +0000

    modified if statement of num_data, kernel_width
    git-svn-id: http://alma-dms/svn/sakura/trunk@876 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/gen_convolution.cc
+++ libsakura/src/gen_convolution.cc
@@ -16,7 +16,7 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(CreateConvolve1DContext)(
 		size_t kernel_width, bool use_fft,
 		LIBSAKURA_SYMBOL(Convolve1DContext) **context) {
 	*context = nullptr;
-	if (!(num_data > 0) || !(num_data <= INT_MAX)) {
+	if (!(0 < num_data && num_data <= INT_MAX)) {
 		LOG4CXX_ERROR(logger, "num_data must be >0 and <= INT_MAX");
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	}
@@ -27,7 +27,7 @@ extern "C" LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(CreateConvolve1DContext)(
 		LOG4CXX_ERROR(logger, "Invalid Kernel Type");
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	}
-	if (!(kernel_width > 0) || !(kernel_width <= INT_MAX)) {
+	if (!(0 < kernel_width && kernel_width <= INT_MAX)) {
 		LOG4CXX_ERROR(logger, "kernel_width must be >0 and <= INT_MAX");
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	}
@@ -59,7 +59,7 @@ LIBSAKURA_SYMBOL(Convolve1DContext) const *context, size_t num_data,
 	if (context == nullptr) {
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	}
-	if (!(num_data > 0) || !(num_data <= INT_MAX)) {
+	if (!(0 < num_data && num_data <= INT_MAX)) {
 		LOG4CXX_ERROR(logger, "num_data must be >0 and <= INT_MAX");
 		return LIBSAKURA_SYMBOL(Status_kInvalidArgument);
 	}
