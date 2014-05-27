88c76ee1d6a32ee1ae6cff13491815dbd885a610
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2014-05-27 04:17:29 +0000

    made test.sh flexible
    git-svn-id: http://alma-dms/svn/sakura/trunk@951 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/python-binding/test.sh
+++ libsakura/python-binding/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=/opt/share/gcc/lib64:/opt/share/casa/current/release/lib64
-exec /opt/share/casa/current/release/lib64/casapy/bin/python test.py
+exec /opt/share/casa/current/release/lib64/casapy/bin/python "$@"
