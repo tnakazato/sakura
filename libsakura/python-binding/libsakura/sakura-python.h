44c8d3956b736fcb5d0de69eddd4d2d5d17de9cc
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2014-05-28 04:27:58 +0000

    comment and doxygen related fixes
    git-svn-id: http://alma-dms/svn/sakura/trunk@955 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/python-binding/libsakura/sakura-python.h
+++ libsakura/python-binding/libsakura/sakura-python.h
@@ -62,11 +62,12 @@ LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferEncapsulate)(
 
 /**
  * @~japanese
- * @brief PyObject(PyCapsule) を @ref PyAlignedBuffer にデカプセル化する
+ * @brief @a PyObject(@a PyCapsule) から、 @a sakura_PyAlignedBuffer を取り出す(デカプセルする)
  *
  * @param[in] capsule デカプセル化する @a PyObject。
- * @param[in] buffer 成功した場合は、デカプセル化された@ref sakura_PyAlignedBuffer。それ以外の場合は不定。
- * デカプセル化に成功した場合は、capsuleが存続する間だけ@ref PyAlignedBufferは有効である。
+ * @param[out] buffer 成功した場合は、デカプセル化された@a sakura_PyAlignedBuffer。それ以外の場合は不定。
+ * デカプセル化に成功した場合は、@a capsule が存続する間だけ@a sakura_PyAlignedBuffer は有効である。
+ * @a buffer は@a capsule が所有したままなので、勝手に@a buffer を解放してはならない。
  * @return 終了ステータス
  */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferDecapsulate)(
@@ -75,9 +76,16 @@ LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferDecapsulate)(
 
 /**
  * @~japanese
- * @brief @ref PyAlignedBuffer を作成する
+ * @brief @ref sakura_PyAlignedBuffer を作成する
  *
+ * @param[in] type 要素の型。signed/unsignedは、無視。
+ * @param[in] original_addr 領域の先頭アドレス。
+ * @param[in] aligned_addr 領域中のアラインされたアドレス。
+ * このアドレスの利用者間で合意されていれば、必ずしもアラインされていなくてもよい(単に@a original_addr と同じアドレスでもよい)。
+ * @param[in] dimensions 次元数(@a elementsの要素数)。現在の実装では、最大次元数は5である。
  * @param[in] elements 各次元の要素数。どういう順序で格納するかは、利用者の自由。
+ * @param[in] destructor @a sakura_PyAlignedBuffer が破棄されるときに @a original_addr を解放するために呼び出されるデストラクター。
+ * @param[out] buffer @a sakura_PyAlignedBuffer の作成に成功した場合は、そのアドレスが格納される。その他の場合は不定。
  * @return 終了ステータス
  */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferCreate)(
@@ -86,27 +94,61 @@ LIBSAKURA_SYMBOL(PyTypeId) type, void *original_addr, void *aligned_addr,
 		LIBSAKURA_SYMBOL(PyAlignedBuffer) **buffer)
 LIBSAKURA_WARN_UNUSED_RESULT;
 
+/**
+ * @~japanese
+ * @brief @ref sakura_PyAlignedBuffer を破棄する
+ * @ref sakura_PyAlignedBufferCreate で指定した@a original_addrは、@a destructorにより、共に破棄される。
+ * @a buffer がNULLの場合は、何もしない。
+ *
+ * @param[in] buffer @a sakura_PyAlignedBuffer のアドレス。
+ */
 void LIBSAKURA_SYMBOL(PyAlignedBufferDestroy)(
 LIBSAKURA_SYMBOL(PyAlignedBuffer) *buffer);
 
+/**
+ * @~japanese
+ * @brief @ref sakura_PyAlignedBuffer から次元数を取得する
+ *
+ * @param[in] buffer @a sakura_PyAlignedBuffer のアドレス。
+ * @param[out] dimensions 成功した場合は、次元数が格納される。その他の場合は不定。
+ * @return 終了ステータス
+ */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferDimensions)(
 LIBSAKURA_SYMBOL(PyAlignedBuffer) *buffer, size_t *dimensions);
 
 /**
  * @~japanese
- * @brief @ref PyAlignedBuffer から各次元の要素数を取得する
+ * @brief @ref sakura_PyAlignedBuffer から各次元の要素数を取得する
  *
+ * @param[in] buffer @a sakura_PyAlignedBuffer のアドレス。
  * @param[in] dimensions	各次元の要素数を先頭から何個取り出したいか(@a elementsに用意されている要素の数)。
- * @a sakura_PyAlignedBufferCreate を呼び出した際の@a dimensions 以下であること。
+ * @ref sakura_PyAlignedBufferCreate を呼び出した際の@a dimensions 以下であること。
+ * @param[out] elements 成功した場合は、各次元の要素数が格納される。その他の場合は不定。
  * @return 終了ステータス
  */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferElements)(
 LIBSAKURA_SYMBOL(PyAlignedBuffer) *buffer, size_t dimensions,
 		size_t elements[]);
 
+/**
+ * @~japanese
+ * @brief @ref sakura_PyAlignedBuffer からアラインされたアドレスを取得する
+ *
+ * @param[in] buffer @a sakura_PyAlignedBuffer のアドレス。
+ * @param[out] aligned_addr 成功した場合は、アラインされたアドレスが格納される。その他の場合は不定。
+ * @return 終了ステータス
+ */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferAlignedAddr)(
 LIBSAKURA_SYMBOL(PyAlignedBuffer) *buffer, void **aligned_addr);
 
+/**
+ * @~japanese
+ * @brief @ref sakura_PyAlignedBuffer から要素の型を取得する
+ *
+ * @param[in] buffer @a sakura_PyAlignedBuffer のアドレス。
+ * @param[out] type 成功した場合は、要素の型が格納される。その他の場合は不定。
+ * @return 終了ステータス
+ */
 LIBSAKURA_SYMBOL(Status) LIBSAKURA_SYMBOL(PyAlignedBufferType)(
 LIBSAKURA_SYMBOL(PyAlignedBuffer) *buffer, LIBSAKURA_SYMBOL(PyTypeId) *type);
 
