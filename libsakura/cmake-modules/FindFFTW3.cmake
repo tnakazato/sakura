907a4aa23bb09c2187faca53092cae44597e9987
nakazato <nakazato@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2014-10-15 18:54:06 +0000

    Defined FFTW3_ROOT_DIR
    git-svn-id: http://alma-dms/svn/sakura/trunk@1067 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/cmake-modules/FindFFTW3.cmake
+++ libsakura/cmake-modules/FindFFTW3.cmake
@@ -5,12 +5,23 @@
 #FFTW3_LIBRARIES where is library
 #FFTW3_DEFINITIONS other options
 
+message(STATUS "FFTW3_ROOT_DIR=${FFTW3_ROOT_DIR}")
+
 #to find header file and library of fftw3
+find_path(FFTW3_INCLUDE_DIR fftw3.h
+	PATHS ${FFTW3_ROOT_DIR}
+	PATH_SUFFIXES include
+	NO_DEFAULT_PATH)
 find_path(FFTW3_INCLUDE_DIR fftw3.h)
+message(STATUS "FFTW3_INCLUDE_DIR=${FFTW3_INCLUDE_DIR}")
 # support for multiple fftw3 libraries
 # set multiple library names to _components, e.g., set(_components fftw3f fftw3)
 set(_components fftw3f)
 foreach(_comp ${_components})
+	find_library(${_comp}_LIBRARY ${_comp}
+		PATHS ${FFTW3_ROOT_DIR} 
+		PATH_SUFFIXES lib64 lib
+		NO_DEFAULT_PATH)
 	find_library(${_comp}_LIBRARY ${_comp})
 	if (${_comp}_LIBRARY)
 		list(APPEND FFTW3_LIBRARIES ${${_comp}_LIBRARY}) 
