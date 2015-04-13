6ad63fbeb8eafe5a7977ac6dae859c29ade63050
wataru <wataru@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2015-04-13 08:50:34 +0000

    fixed comment for basis_data as data of the j-th basis function at the i-th point is stored at basis_data[num_bases * i + j] (previously written like basis_data[num_bases * j + i]).
    git-svn-id: http://alma-dms/svn/sakura/trunk@1353 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/baseline.h
+++ libsakura/src/baseline.h
@@ -38,7 +38,7 @@ struct LIBSAKURA_SYMBOL(BaselineContext) {
 	size_t num_bases; /**< ベースラインモデルを構成する基底関数の数 */
 	size_t num_basis_data; /**< 個々の基底関数を表現するデータ点の数 */
 	void *basis_data_storage; /**< 基底関数データを格納する領域を確保した時に返されるアドレス。アラインされておらず、解放時に用いる目的のみのために保持される。 */
-	double *basis_data; /**< 基底関数データを指すポインタ。アドレスはアラインされている。要素数は @a num_bases * @a num_basis_data 。i 番目の基底関数の j 番目の点のデータは [ @a num_basis_data * i + j ] 番目の要素に格納される。 */
+	double *basis_data; /**< 基底関数データを指すポインタ。アドレスはアラインされている。要素数は @a num_bases * @a num_basis_data 。i 番目の点の j 番目の基底関数のデータは [ @a num_basis_data * i + j ] 番目の要素に格納される。 */
 };
 }
 
