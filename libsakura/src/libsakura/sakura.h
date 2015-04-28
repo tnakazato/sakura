e9db2d0cba585010dd3300b0c09a2d9a314d7fb6
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2015-04-28 05:05:39 +0000

    comment update
    git-svn-id: http://alma-dms/svn/sakura/trunk@1549 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/src/libsakura/sakura.h
+++ libsakura/src/libsakura/sakura.h
@@ -467,6 +467,8 @@ typedef struct {
  * an average of two center values.
  * The caller is responsible to take a median value from @a new_data as
  * a median absolute deviation.
+ *
+ *  mad = (@a new_data[@a num_data/2] + @a num_data[@a num_data/2 - @a num_data%2]) / 2 if @a num_data > 0
   *
  * @param[in] num_data	The number of elements in @a data and @a new_data .
  * @param[in] data	Each value in data must not be Inf nor NaN. @a data must be sorted.
