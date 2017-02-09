0277af1ef6942e585bcd816bea25288a736e19d0
kohji <kohji@9a79bd28-2a0b-46f8-beb7-14fc758f186d>
2017-02-09 00:50:34 +0000

    add scope qualifier to support gcc 5.4
    git-svn-id: http://alma-dms/svn/sakura/trunk@2106 9a79bd28-2a0b-46f8-beb7-14fc758f186d

--- libsakura/test/statistics.cc
+++ libsakura/test/statistics.cc
@@ -73,7 +73,7 @@ LIBSAKURA_SYMBOL(StatisticsResultFloat) *result) {
 template<typename T>
 void ExpectEQ(T const &ref, T const &result) {
 	if (std::isnan(ref)) {
-		EXPECT_TRUE(isnan(result));
+		EXPECT_TRUE(std::isnan(result));
 	} else {
 		EXPECT_EQ(ref, result);
 	}
@@ -87,7 +87,7 @@ void ExpectRealEQ(T const &ref, T const &result) {
 template<>
 void ExpectRealEQ<double>(double const &ref, double const &result) {
 	if (std::isnan(ref)) {
-		EXPECT_TRUE(isnan(result));
+		EXPECT_TRUE(std::isnan(result));
 	} else {
 		EXPECT_DOUBLE_EQ(ref, result);
 	}
@@ -144,12 +144,12 @@ bool CallAndTestResult(LIBSAKURA_SYMBOL(StatisticsResultFloat) const &ref,
 	auto min_index_ptr = &decltype(result)::index_of_min;
 	auto min_max_test =
 			[&data, &result, &result_accurate](decltype(min_ptr) attr, decltype(min_index_ptr) index) {
-				if (isnan(result.*attr)) {
-					EXPECT_TRUE(isnan(result_accurate.*attr));
+				if (std::isnan(result.*attr)) {
+					EXPECT_TRUE(std::isnan(result_accurate.*attr));
 					EXPECT_EQ(-1, result.*index);
 					EXPECT_EQ(-1, result_accurate.*index);
 				} else {
-					EXPECT_FALSE(isnan(result_accurate.*attr));
+					EXPECT_FALSE(std::isnan(result_accurate.*attr));
 					EXPECT_EQ(result.*attr, result_accurate.*attr);
 					EXPECT_LE(0, result.*index);
 					EXPECT_LE(0, result_accurate.*index);
@@ -171,18 +171,18 @@ bool CallAndTestResult(LIBSAKURA_SYMBOL(StatisticsResultFloat) const &ref,
 	EXPECT_EQ(result.count, result_accurate.count);
 	auto value_test =
 			[&result, &result_accurate](decltype(&decltype(result)::sum) attr) {
-				if (isnan(result.*attr)) {
-					EXPECT_TRUE(isnan(result_accurate.*attr));
+				if (std::isnan(result.*attr)) {
+					EXPECT_TRUE(std::isnan(result_accurate.*attr));
 				} else {
-					EXPECT_TRUE(!isnan(result_accurate.*attr));
+					EXPECT_TRUE(!std::isnan(result_accurate.*attr));
 				}
 			};
 	value_test(&decltype(result)::sum);
 	value_test(&decltype(result)::square_sum);
 
-	if ((!isnan(result.sum) && !isnan(result_accurate.sum)
+	if ((!std::isnan(result.sum) && !std::isnan(result_accurate.sum)
 			&& result.sum != result_accurate.sum)
-			|| (!isnan(result.square_sum) && !isnan(result_accurate.square_sum)
+			|| (!std::isnan(result.square_sum) && !std::isnan(result_accurate.square_sum)
 					&& result.square_sum != result_accurate.square_sum)) {
 		if (false) { // for debug
 			std::cout << "error ratio: \n";
@@ -235,7 +235,7 @@ TEST(Statistics, SortValidValuesDensely) {
 		result = LIBSAKURA_SYMBOL(SortValidValuesDenselyFloat)(0, is_valid,
 				data, &new_elements);
 		EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), result);
-		EXPECT_EQ(0, new_elements);
+		EXPECT_EQ(0U, new_elements);
 	}
 	{
 		SIMD_ALIGN
@@ -264,7 +264,7 @@ TEST(Statistics, SortValidValuesDensely) {
 		result = LIBSAKURA_SYMBOL(SortValidValuesDenselyFloat)(ELEMENTSOF(data),
 				is_valid, data, &new_elements);
 		EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), result);
-		EXPECT_EQ(0, new_elements);
+		EXPECT_EQ(0U, new_elements);
 	}
 	{
 		SIMD_ALIGN
@@ -703,7 +703,7 @@ TEST(Statistics, ComputeStatistics_Accuracy) {
 			if (CallAndIsDifferent(i, data.data(), is_valid.data())) {
 				//std::cout << "ERROR index: " << i << std::endl;
 				EXPECT_LT(start, i); // if this fails, make 'start' more smaller value.
-				EXPECT_LE(11000, i);
+				EXPECT_LE(size_t(11000), i);
 				break;
 			}
 		}
@@ -715,7 +715,7 @@ TEST(Statistics, ComputeStatistics_Accuracy) {
 					data.data(), is_valid.data(), &result);
 			EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
 		}
-		EXPECT_LT(0, result.count);
+		EXPECT_LT(0U, result.count);
 
 		EXPECT_EQ(mean, result.sum / result.count);
 		double stddev = 0;
@@ -831,7 +831,7 @@ TEST(Statistics, ComputeStatistics_Accuracy) {
 			EXPECT_EQ(LIBSAKURA_SYMBOL(Status_kOK), status);
 		}
 
-		EXPECT_LT(0, result.count);
+		EXPECT_LT(0U, result.count);
 
 		EXPECT_EQ(mean, result.sum / result.count);
 		double stddev = 0;
