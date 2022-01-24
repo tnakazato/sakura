b69fc318942d81751ef691bc01fb0374e179ee8e
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2022-01-24 11:16:09 +0900

    update copyright notice according to the guideline

--- libsakura/python-binding/test.sh
+++ libsakura/python-binding/test.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # @SAKURA_LICENSE_HEADER_START@
-# Copyright (C) 2013-2016
-# National Astronomical Observatory of Japan
+# Copyright (C) 2013-2022
+# Inter-University Research Institute Corporation, National Institutes of Natural Sciences
 # 2-21-1, Osawa, Mitaka, Tokyo, 181-8588, Japan.
 # 
 # This file is part of Sakura.
 # 
-# Sakura is free software: you can redistribute it and/or modify it under 
-# the terms of the GNU Lesser General Public License as published by the 
-# Free Software Foundation, either version 3 of the License, or (at your 
+# Sakura is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 # 
-# Sakura is distributed in the hope that it will be useful, but WITHOUT 
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public 
+# Sakura is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 # License for more details.
 # 
-# You should have received a copy of the GNU Lesser General Public License 
+# You should have received a copy of the GNU Lesser General Public License
 # along with Sakura.  If not, see <http://www.gnu.org/licenses/>.
 # @SAKURA_LICENSE_HEADER_END@
 
